6fd7fe360e63decd65be29553403a09c84ef6edb
akarsh003 <akarshakr2000@gmail.com>
2021-11-13 19:29:43 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css'
 import Homepage from './componente/homepage/homepage'
 import Login from './componente/login/login'
 import Register from './componente/register/register'
+import { BrowserRouter, Route, Routes , Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage/> */}
-      <Login/>
-       {/*  <Register/> */}
-
-
+		<BrowserRouter>
+        <Routes >
+			<Route exact path="/"
+				render={() => {
+					return (<Navigate to="/home" /> )
+				}}/>
+        	<Route path="/home" element={<Homepage/>}/>
+			<Route path="/login" element={<Login/>}/>
+        	<Route path="/register" element={<Register/>}/>
+        </Routes >
+      </BrowserRouter>
     </div>
   );
 }
--- src/componente/homepage/homepage.js
+++ src/componente/homepage/homepage.js
@@ -3,8 +3,8 @@ import "./homepage.css"
 
 const Homepage = () => {
     return (
-        <div className="homepage"> 
-        <h1>hello Homepage</h1>
+        <div className="homepage">
+       	 <h1>hello Homepage</h1>
         <div className="button"> logout </div>
         </div>
     )
